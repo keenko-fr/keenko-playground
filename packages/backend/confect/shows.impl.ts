a2ddff59bb29c31010447e0f5f64e9048e424797
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2026-09-02 15:24:57 +0400

    fix: provide tvmaze at confect boundary

--- packages/backend/confect/shows.impl.ts
+++ packages/backend/confect/shows.impl.ts
@@ -1,5 +1,5 @@
 import { FunctionImpl, GroupImpl } from "@confect/server";
-import { Layer } from "effect";
+import { Effect as E, Layer } from "effect";
 import { FetchHttpClient } from "effect/unstable/http";
 
 import * as features from "../features/shows";
@@ -8,9 +8,8 @@ import databaseSchema from "./_generated/schema";
 import shows from "./shows.spec";
 
 // ACTIONS ---------------------------------------------------------------------------------------------------------------------------------
-const searchImpl = FunctionImpl.make(databaseSchema, shows, "search", ({ query }) => features.search(query)).pipe(
-  Layer.provide(TvMaze.layer),
-  Layer.provide(FetchHttpClient.layer)
+const searchImpl = FunctionImpl.make(databaseSchema, shows, "search", ({ query }) =>
+  features.search(query).pipe(E.provide(TvMaze.layer), E.provide(FetchHttpClient.layer))
 );
 
 export default GroupImpl.make(databaseSchema, shows).pipe(Layer.provide(searchImpl), GroupImpl.finalize);
